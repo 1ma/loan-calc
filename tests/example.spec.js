304efdd05e7d3f2646a81fec0c7d35bef9feff17
Marcel Hernandez <e6990620@gmail.com>
2024-10-02 23:13:34 +0200

    first real end to end test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,18 +1,17 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('smoke test with default values', async ({page}) => {
+    await page.goto('/');
+    await expect(page.locator('input#loanAmountLabel')).toHaveValue('12000');
+    await expect(page.locator('input#interestRateLabel')).toHaveValue('2.5');
+    await expect(page.locator('input#loanPeriodLabel')).toHaveValue('10');
+    await expect(page.locator('select#loanPeriodType option:checked')).toHaveText('Years');
+    await expect(page.locator('input#monthlyFeeLabel')).toHaveValue('0');
+    await expect(page.locator('input#startingFeeLabel')).toHaveValue('0');
+    await expect(page.locator('[js-total-payments]')).toHaveText('13,574.87');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    await page.getByText('Table', {exact: true}).click();
+    await expect(page.locator('[js-breakdown-items] > div')).toHaveCount(120);
+    await expect(page.locator('[js-breakdown-items] > div:first-child')).toContainText("25.00€");
+    await expect(page.locator('[js-breakdown-items] > div:last-child')).toContainText("0.24€");
 });
